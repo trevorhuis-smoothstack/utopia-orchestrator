95792c323bc84b1e80dc8fc9e0bb02239009d721
trevorhuis-smoothstack <trevor.huis@Smoothstack.com>
2020-06-12 17:11:19 -0500

    intitial commit

---
+++ src/test/java/com/ss/training/utopia/orchestrator/OrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.training.utopia.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
