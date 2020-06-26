22826d2e3845079200c0001a46d4d48630280f8a
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-26 17:45:26 -0600

    Add user details service class for counter

---
+++ src/main/java/com/ss/training/utopia/orchestrator/counter/security/UserDetailsServiceClass.java
@@ -0,0 +1,29 @@
+package com.ss.training.utopia.orchestrator.counter.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ss.training.utopia.orchestrator.security.User;
+
+/**
+ * @author Justin O'Brien
+ */
+@Service
+public class UserDetailsServiceClass implements UserDetailsService {
+
+    @Autowired
+	UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+		return new UserDetailsClass(
+				user.orElseThrow(() -> new UsernameNotFoundException("The user '" + username + "' was not found.")));
+    }
+    
+}
