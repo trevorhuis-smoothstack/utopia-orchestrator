9e21ca687b7d7ec605b25ecd041cf349e59304e9
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-26 18:09:06 -0600

    Counter: Add authorization filter

---
+++ src/main/java/com/ss/training/utopia/orchestrator/counter/security/AuthorizationFilter.java
@@ -0,0 +1,69 @@
+package com.ss.training.utopia.orchestrator.counter.security;
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.ss.training.utopia.orchestrator.security.User;
+
+/**
+ * @author Justin O'Brien
+ */
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+	private UserRepository userRepository;
+
+	/**
+	 * @param authenticationManager
+	 * @param userRepository
+	 */
+	public AuthorizationFilter(AuthenticationManager authenticationManager, UserRepository userRepository) {
+		super(authenticationManager);
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String header = request.getHeader(JwtProperties.HEADER_STRING);
+		Authentication authentication;
+		if (header != null && header.startsWith(JwtProperties.TOKEN_PREFIX)) {
+			authentication = getUsernamePasswordAuthentication(request);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+		chain.doFilter(request, response);
+	}
+
+	private Authentication getUsernamePasswordAuthentication(HttpServletRequest request)
+			throws UsernameNotFoundException {
+		String token = request.getHeader(JwtProperties.HEADER_STRING), username;
+		UsernamePasswordAuthenticationToken authToken;
+		User user;
+		UserDetailsClass userDetails;
+		if (token != null) {
+			username = JWT.require(HMAC512(JwtProperties.SECRET.getBytes())).build()
+					.verify(token.replace(JwtProperties.TOKEN_PREFIX, "")).getSubject();
+			if (username != null) {
+				user = userRepository.findByUsername(username)
+						.orElseThrow(() -> new UsernameNotFoundException("The user '" + username + "' was not found."));
+				userDetails = new UserDetailsClass(user);
+				authToken = new UsernamePasswordAuthenticationToken(username, null, userDetails.getAuthorities());
+				return authToken;
+			}
+		}
+		return null;
+	}
+
+}
