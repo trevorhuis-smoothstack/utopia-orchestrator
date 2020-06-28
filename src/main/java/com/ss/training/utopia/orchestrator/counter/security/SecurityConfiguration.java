a7717d77a203da2dd8cf1aa63b97ccf15372cae0
Justin O'Brien <justin.obrien@smoothstack.com>
2020-06-28 10:10:18 -0600

    Expose authorization header

--- src/main/java/com/ss/training/utopia/orchestrator/counter/security/SecurityConfiguration.java
+++ src/main/java/com/ss/training/utopia/orchestrator/counter/security/SecurityConfiguration.java
@@ -71,7 +71,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
