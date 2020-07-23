d4155be6d5d7f3c0afd05e1289ff273ba1e1dcc9
sean-darsie <sean.darsie@smoothstack.com>
2020-07-23 11:13:57 -0700

    hide jwt secret

--- src/main/java/com/ss/training/utopia/orchestrator/security/JwtProperties.java
+++ src/main/java/com/ss/training/utopia/orchestrator/security/JwtProperties.java
@@ -5,9 +5,8 @@ package com.ss.training.utopia.orchestrator.security;
  */
 public class JwtProperties {
 
-	public static final String SECRET = "SmoothstackJava042020UTOPIA";
+	public static final String SECRET = System.getenv("JWT_SECRET"); // "SmoothstackJava042020UTOPIA";
     public static final int EXPIRATION_TIME = 86_400_000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-
 }
